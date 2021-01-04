da48760f891f78ce2b4607151e0830e10fcd0de6
Yoni Gang <yonigang1@gmail.com>
2021-01-05 00:44:11 +0200

    some basice route added

--- polls_app/frontend/src/App.js
+++ polls_app/frontend/src/App.js
@@ -1,25 +1,19 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import App2 from './app2';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={App2}/>
+          </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
